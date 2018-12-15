234872f916e68d5cf600d6c8b807ef3159767a42
IIZODIACII <amr_x14@outlook.com>
2018-12-15 23:34:01 +0200

    Added Comments

--- Priority_Scheduler.java
+++ Priority_Scheduler.java
@@ -12,12 +12,12 @@ import java.util.PriorityQueue;
 public class Priority_Scheduler {
 
     PriorityQueue<Process> processes = new PriorityQueue<Process>(new Comp());
-    ArrayList<Process> arr = new ArrayList<Process>();
-    ArrayList<Process> working = new ArrayList<Process>();
+    ArrayList<Process> arr = new ArrayList<Process>(); // an array of processes
+    ArrayList<Process> working = new ArrayList<Process>(); // an array of the running processes the last process added in this array is the currently running one
     private int ContextSwitch;
-    private int limit = 2; // reduce the priority of a waiting process every 2 units of time
-    private boolean add = true;
-    private boolean sw = false;
+    private int limit = 2; // reduce the priority of a waiting process every "limit" units of time -Aging Technique - to solve the starvation problem
+    private boolean add = true; // if the running process finished its work there should be a new process running the "add" indicates that a new process should be running now
+    private boolean sw = false; // indicates that there is a process with hiegher priority should run now instead of the current one -the lower the number the heigher the priority-.
 
     Priority_Scheduler(){}
     Priority_Scheduler(ArrayList prs, int t) {
@@ -29,14 +29,14 @@ public class Priority_Scheduler {
     }
 
     public void Start(){
-        Process nxt = arr.get(0);
+        Process nxt = arr.get(0); // the next process that should be running
         int time = 0;
-        int idx = 0;
+        int idx = 0; // the index of the currently running process
 
         while (processes.size() > 0) {
-            if (sw) {
-                if (nxt.get_ArrTime() > time)
-                    time += ContextSwitch + (nxt.get_ArrTime() - time);
+            if (sw) { // if there is aa process that should switch and run instead of the currently running process
+                if (nxt.get_ArrTime() > time) // if the new process didn't arrive yet
+                    time += ContextSwitch + (nxt.get_ArrTime() - time); // add the ContextSwitch and add the remaining time untill the new process arrives
                 else {
                     if(ContextSwitch != 0)
                         time += ContextSwitch;
@@ -47,48 +47,49 @@ public class Priority_Scheduler {
             else
                 time++;
 
-            if (add || sw) {
+            if (add || sw) { // if there is a switch or just a new process should start running now
                 working.add(nxt);
                 nxt.set_rt(time);
-                sw = false;
-                add = false;
+                sw = false; // set false so the process won't add itself again in the next iteration
+                add = false; // set false so the process won't add itself again in the next iteration
             }
 
-            for (Process p : processes) {
-                if (p != nxt && p.get_ArrTime() <= time && p.get_pr() > 1 && p.get_rt() > 1){
-                    if (time - p.get_rt() >= limit)
+            for (Process p : processes) { // Loop through the processes
+                if (p != nxt && p.get_ArrTime() <= time && p.get_pr() > 1 && p.get_rt() > 1){ // if the p is not the current running process && the pr. arrived && the priority wasn't reduced before
+                    if (time - p.get_rt() >= limit) // if it passed limit unit of time and the process's priority wasn't reduced
                         p.set_pr(p.get_pr() - 1);
                 }
-                else if(p != nxt && p.get_ArrTime() <= time && p.get_pr() > 1 && time - p.get_ArrTime() >= limit) {
-                    p.set_pr(p.get_pr() - 1);
-                    p.set_rt(time);
+                else if(p != nxt && p.get_ArrTime() <= time && p.get_pr() > 1 && time - p.get_ArrTime() >= limit) { // if the p is not the current running process && the pr. arrived
+                    p.set_pr(p.get_pr() - 1); // reduce the priority
+                    p.set_rt(time); // the priority was reduced at this time so it will not reduce again in the next iteration, it will wait "limit" unit of time to be reduced again
                 }
             }
 
 
-            Process n = Get_Nxt(nxt, idx, time); // check for switch
-            if (n != nxt) {
-                idx++;
+            Process n = Get_Nxt(nxt, time); // check for switch
+            if (n != nxt) { // if the n is not the currently running process then there should be a switch
+                idx++; // point to the new process and add it in the next iteration
                 nxt = n;
-                if (sw) {
-                    int t = idx - 1;
-                    Process temp = working.get(t);
 
-                    if (temp.get_BTime() > 0)
+                if (sw) {
+                    int t = idx - 1; // get the index of the currently running process
+                    Process temp = working.get(t); // get the currently running process
+
+                    if (temp.get_BTime() > 0) // if it didn't finish reduce its BurstTime
                         temp.set_BTime(temp.get_BTime() - 1);
 
-                    if (temp.get_BTime() == 0) {
+                    if (temp.get_BTime() == 0) {// if it is done calculate the required values
                         temp.set_ct(time);
                         temp.set_tat(temp.get_ct() - temp.get_ArrTime());
                         temp.set_waiting(temp.get_tat() - temp.get_CBTime());
 
-                        processes.removeIf(Process -> temp.get_id() == Process.get_id());
+                        processes.removeIf(Process -> temp.get_id() == Process.get_id()); // remove the process from the queue -the process may not be at the top of the queue-
                     }
 
-                    working.set(t, temp);
+                    working.set(t, temp); // replace the currently working process with the modified copy
                 }
             }
-            else {
+            else { // if there is no switch
                 int t = idx;
                 Process temp = working.get(t);
 
@@ -100,9 +101,9 @@ public class Priority_Scheduler {
                     temp.set_tat(temp.get_ct() - temp.get_ArrTime());
                     temp.set_waiting(temp.get_tat() - temp.get_CBTime());
 
-                    processes.poll();
+                    processes.poll(); // remove the process from the queue -the process is always at the top of the queue-
 
-                    Process ne = Get_Nxt(nxt, idx, time); // check for new process
+                    Process ne = Get_Nxt(nxt, time); // get a new process to run it
                     if (nxt != ne) {
                         idx++;
                         nxt = ne;
@@ -115,17 +116,18 @@ public class Priority_Scheduler {
         print();
     }
 
-    private Process Get_Nxt(Process nxt, int idx, int time){
-        Process same = nxt;
+    private Process Get_Nxt(Process nxt, int time){
+        Process same = nxt; // checks if nxt hasn't changed
+
         for(Process pr : processes) {
-            if (pr.get_ArrTime() <= time && pr.get_BTime() > 0) {
-                if(nxt.get_BTime() == 0) {
+            if (pr.get_ArrTime() <= time && pr.get_BTime() > 0) { // if the process has arrived and is hasn't finish its work
+                if(nxt.get_BTime() == 0) { // if the currently running process has finished its work get a new one
                     nxt = pr;
                     add = true;
                     sw = false;
                     break;
                 }
-                else if (pr.get_pr() <= nxt.get_pr() && pr != nxt){
+                else if (pr.get_pr() <= nxt.get_pr() && pr != nxt){ // if the currently running process priority is lower than another process
                     nxt = pr;
                     sw = true;
                     add = false;
@@ -133,7 +135,7 @@ public class Priority_Scheduler {
                 }
             }
         }
-        if (same == nxt){
+        if (same == nxt){ // if nxt hasn't changed then there is no switch and there is no new process to run
             add = false;
             sw = false;
         }
