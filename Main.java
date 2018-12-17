45e114a885d7260f080c5db80d3b3daea2bb8fe9
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-12-17 18:22:28 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner Input=new Scanner(System.in);
+	System.out.println(Math.ceil(5*0.25));
+		System.out.println(5*0.25);
+		
+		
+		Input.close();		
+		
+	}
+
+}
