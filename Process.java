ae5071c33e14fae36fe738c3e9175a9dac83f07a
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-12-17 18:07:05 +0200

    Add files via upload

---
+++ Process.java
@@ -0,0 +1,19 @@
+package b;
+
+public class Process {
+	
+String Name;
+int Burst;
+int Quantum;
+int Arrival;	
+int Priorty;	
+int Index;
+public Process(String Name,int Burst,int Quantum,int Arrival,int priorty,int Index){
+this.Name=Name;
+this.Burst=Burst;
+this.Quantum=Quantum;
+this.Arrival=Arrival;
+this.Priorty=priorty;
+this.Index=Index;
+	     }
+}
